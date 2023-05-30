9ea516ae577178dc45445ef8e8870642136b8ce4
emmaMarful <eamarful@gmail.com>
2023-05-30 10:30:55 +0000

    add lines to the pdf

--- main.py
+++ main.py
@@ -13,16 +13,34 @@ for index, row in dataFrame.iterrows():
     pdf.line(10, 26, 200, 26)
     pdf.cell(w=0, h=24, txt=row['Topic'], align="L", ln=1)
 
-    pdf.ln(243)  # a break line for fotter
+    # lines. Page One
+    lnn = 27
+    nextLine = 26
+    for i in range(lnn):
+        pdf.line(10, nextLine, 200, nextLine)
+        nextLine = nextLine + 10
+
+    # a break line for footer. Page One
+    pdf.ln(254)
     pdf.set_font(family="Times", style="I", size=8)
     pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
 
+    # additional iterated pages
     pages = row['Pages'] - 1
     for i in range(pages):
         pdf.add_page()
-        pdf.ln(266)
+        pdf.ln(280)
         pdf.set_font(family="Times", style="I", size=8)
         pdf.cell(w=0, h=8, align="R", txt=row["Topic"])
+        pdf.line(10, 15, 200, 15)
+
+        lnn = 250
+        nextLine2 = 30
+        for l in range(lnn):
+            pdf.line(10, nextLine2, 200, nextLine2)
+            nextLine2 = nextLine2 + 10
+
+
 
 
 pdf.output("output.pdf")
